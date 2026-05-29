bd8a373d1ba3ca04b74ff2575af5ab0c9fdd6781
Michael Stewart <stew.mich258@gmail.com>
2026-05-29 11:54:15 -0400

    remove wasm stuff, and weird backup file

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ use winit::{
     window::Window,
     };
 
+// stop giving errors. from me...to compiler...with love
+fn main() {}
+
+
 pub struct State { 
     window: Arc<Window>,
 }
@@ -15,19 +19,10 @@ pub struct State {
 
 // the reason state is an option is that we need to be in resumed state to create a window, maybe
 // like a pause menu
-//
-// The proxy resource is used for creating wgpu processes on web, which is async.
-// consider removing this since the plan for this game is to run as a process (like regular games)
-// not on the web. 
-
 
 pub struct App {
-    #[cfg(target_arch = "wasm32")]
-    proxy: Option<winit::event_loop::EventLoopProxy<State>>,
     state: Option<State>,
 }
-
-
 
 
 // implement a state trait, this will be state machine? 
@@ -38,84 +33,33 @@ impl ApplicationHandler<State> for App {
         #[allow(unused_mut)]
         let mut window_attributes = Window::default_attributes(); 
 
-        // this seems to be only for web applications, I will include it for now but likely delete
-        // it later when optimizing for readability and scope
-        #[cfg(target_arch = "wasm32")]
-        {
-            use wasm::bindgen::JsCast; 
-            use winit::platform::web::WindowAttributesExtWebSys; 
-
-            const CANVAS_ID: &str = "canvas";
-
-            let window = wgpyu::web_sys::window().unwrap_throw(); 
-            let document = window.document().unwrap_throw(); 
-            let canvas = document.get_element_by_id(CANVAS_ID).unwrap_throw(); 
-            let html_canvas_element = canvas.unchecked_into(); 
-            window_attributes = window_attributes.with_canvas(Some(html_canvas_element));
-        }
+        
         // this window data will need to be passed to multiple threads
         let window = Arc::new(event_loop.create_window(window_attributes).unwrap()); 
         
         #[cfg(not(target_arch="wasm32"))]
         {
-            self.state = Some(pollster::block_on(State::new(window).unwrap());
+            self.state = Some(pollster::block_on(State::new(window).unwrap()));
             
+        }}
+
+        #[allow(unused_mut)]
+        fn user_event(&mut self, _event_loop: &ActiveEventLoop, mut event: State) {
+            self.state = Some(event);
         }
-
-        #[cfg(target_arch="wasm32")]
-        {
-            if let Some(proxy) = self.proxy.take() {
-                wasm_bindgen_futures::spawn_local(async move {
-                    assert!(proxy
-                        .send_event(
-                            State::new(window)
-                            .await
-                            .expect("cant create canvas");
-                        }
-                        .is_ok())
-                        });
-                    }
-                }
-            }
-
-            #[allow(unused_mut)]
-            fn user_event(&mut self, _event_loop: &ActiveEventLoop, mut event: State) {
-                #[cfg(target_arch = "wasm32")]
-                {
-                    event.window.request_redraw(); 
-                    event.resize(
-                        event.window.inner_size().width,
-                        event.window.inner_size().height,
-                    );
-                }
-                self.state = Some(event);
-            }
 }
 
-
-
-
-
-
-
-
 impl App {
-    pub  fn new(#[cfg(target_arch = "wasm32")] event_loop: &EventLoop<State>) -> Self {
-        #[cfg(target_arch = "wasm32")]
-        let proxy = Some(event_loop.create_proxy());
+    pub  fn new(event_loop: &EventLoop<State>) -> Self {
         Self {
             state: None,
-            #[cfg(target_arch = "wasm32")]
-            proxy,
         }
     }
 }
 
 impl State { 
     pub async fn new( window : Arc<Window>) -> anyhow::Result<Self> {
-        Ok( Self { 
-            window,
-        })
+        Ok(Self {window})
     }
 
     pub fn resize(&mut self, _width: u32, _height: u32) {
@@ -132,5 +76,5 @@ impl State {
     }
 
 }
-}
+
 
