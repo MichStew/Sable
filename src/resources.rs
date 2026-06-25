b7838b07a537fa1706c3cf05ba72b57e84f4b039
Michael Stewart <michael.stewart@amarok.com>
2026-06-24 20:59:05 -0400

    buggy implementation for hdr render

--- src/resources.rs
+++ src/resources.rs
@@ -2,6 +2,161 @@ use std::io::{BufReader, Cursor};
 use wgpu::util::DeviceExt;
 use crate::{model,texture};
 
+pub struct HdrLoader {
+    texture_format: wgpu::TextureFormat,
+    equirect_layout: wgpu::BindGroupLayout,
+    equirect_to_cubemap: wgpu::ComputePipeline, 
+}
+
+impl HdrLoader {
+    pub fn new(device: &wgpu::Device) -> Self {
+        let module = device.create_shader_module(wgpu::inlude_wgsl!("equirectangular.wgsl"));
+        let texture_format = wgpu::TextureFormat::Rgba32Float;
+        equirect_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("HdrLoader::equirect_layout"),
+            entrier: &[
+                wgpu::BindGroupLayotuEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        multisampled: false,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::StorageTexture {
+                        access: wgpu::StorageTextureAccess::WriteOnly,
+                        format: texture_format,
+                        view_dimension: wgpu::TextureViewDimension::D2Array,
+                    },
+                    count: None,
+                },
+                ],
+        });
+
+        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: None, 
+            bind_group_layouts: &[Some(&equirect_layout)],
+            immediate_size: 0,
+        });
+
+        let equirect_to_cubemap = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            label: Some("equirect_to_cubemap"),
+            layout: Some(&pipeline_layout),
+            module: &module,
+            entry_point: Some("compute_equirect_to_cubemap"),
+            compilation_options: Default::default(),
+            cache: None,
+        });
+
+        Self{
+            equirect_to_cubemap,
+            texture_format,
+            equirect_layout,
+        }
+    }
+
+    pub fn from_equirectangular_bytes(
+        &self,
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        data: &[u8],
+        dst_size: u32,
+        label: Option<&str>,
+    ) -> anyhow::Result<texture::CubeTexture> {
+        let hdr_decoder = HdrDecoder::new(Cursor::new(data))?;
+        let meta = hdr_decoder.metadata();
+
+        let pixels = {
+            let mut pixels = vec![[0.0,0.0,0.0,0.0]; meta.width as usize* meta.height as usize];
+            hdr_decoder.read_image_transform(
+                |pix| {
+                    let rgb = pix.to_hdr();
+                    [rbg.0[0], rgb.0[1], rbg.0[2], 1.0f32]
+                },
+                &mut pixels[..],
+            )?;
+            pixels
+        };
+
+        let src = texture::Texture::create_2d_texture(
+            device, 
+            meta.width,
+            meta.height,
+            self.texture_format,
+            wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+            wgpu::FilterMode::Linear,
+            None,
+        );
+
+        queue.write_texture(
+            wgpu::TexelCopyTexureInfo {
+                texture: &src.texture, 
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+                aspect: wgpu::TextureAspect::All,
+            },
+            &bytemuck::case_slice(&pixels),
+            egpu::TexelCopyBufferLayout {
+                offset: 0,
+                byter_per_row: Some(src.size.width * std::mem::size_of::<[f32; 4]>() as u32),
+                rows_per_image: Some(src.size.height),
+            },
+            src.size,
+        );
+
+        let dst = texture::CubeTexture::create_2d(
+            device,
+            dst_size,
+            dst_size,
+            self.texture_Format,
+            1,
+            wgpu::TextureUsges::STORAGE_BINDING | wgpu::TextureUsages::TEXTURE_BINDING,
+            wgpu::FilterMode::Nearest,
+            label,
+        );
+
+        let dst_view = dst.texture().create_view(&wgpu::TextureViewDescripor { 
+            label,
+            dimension: Some(wgpu::TextureViewDimension::D2Array),
+            ..Default::default()
+        });
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor { 
+            label,
+            layout: &self.equirect_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&src.view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::TextureView(&dst.view),
+                },
+            ],
+        });
+
+        let mut encoder = device.create_command_encoder(&defult::default());
+        let mut pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label });
+
+        let num workgroups = (dst_size + 15) / 16;
+
+        pass.set_pipeline(&self.equirect_to_cubemap);
+        pass.set_bind_group(0, &bind_group, &[]);
+        pass.dispatch_workgrous(num_workgroups, num_workgroups, 6);
+
+        drop(pass); //what?
+
+        queue.submit([encoder.finish()]);
+        Ok(dst)
+    }
+}
+
 
 pub async fn load_string(file_name: &str) -> anyhow::Result<String> {
     let txt = {
